9f1b3569b1e4387f54f180de809c329639679b76
Irfan Ihsanul Amal <irfan.ihsanulamal@yahoo.com>
2016-12-08 15:29:53 +0700

    Add files via upload
    Upload pertama

---
+++ arduinoxandroid.ino
@@ -0,0 +1,20 @@
+
+char data=0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+
+}
+
+void loop() {
+  data = Serial.read();
+  Serial.print(data);
+  Serial.print("/n");
+  if (data==1){
+    digitalWrite(9,HIGH);
+    digitalWrite(10,LOW):}
+  else if (data==2){
+    digitalWrite(9,LOW);
+    digitalWrite(10,HIGH):}
+}
